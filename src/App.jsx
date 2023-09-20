0be41439f643561518d83803397b16c1d891bda6
lftdev <torres.lf21@gmail.com>
2023-09-19 22:06:34 -0300

    Start repository

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Stack } from '@mui/material'
+
+export default function App () {
+  return (
+    <>
+      <Stack component='header' direction='row' justifyContent='space-between' alignItems='center' sx={{ p: 2 }}>
+        <h1>HELLO</h1>
+      </Stack>
+    </>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
+
+const THEME = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#FF525D'
+    },
+    secondary: {
+      main: '#8d8fe9'
+    }
+  }
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={THEME}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+)
