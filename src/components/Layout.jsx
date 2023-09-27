b9c65301fa6bdfd6e863f74d7528ad7e0e7ff1b4
lftdev <torres.lf21@gmail.com>
2023-09-27 09:10:17 -0300

    Add Link to logo

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,6 @@
 import { Link, Stack, Typography, useMediaQuery } from '@mui/material'
+import { Link as RLink } from 'react-router-dom'
+import { APP_NAME, LAYOUT_FOOTER_ATTRIBUTION } from '../constants/strings'
 import CartButton from './CartButton'
 import NavigationMenu from './NavigationMenu'
 import { LOGO_ICON, MAIN_NAV_ICONS } from './SVGIcons'
@@ -17,7 +19,7 @@ export default function Layout (props) {
   return (
     <>
       <Stack component='header' direction='row' justifyContent='space-between' alignItems='center' sx={{ position: 'fixed', top: 0, width: '100%', p: 3 }}>
-        {LOGO_ICON()}
+        <RLink to='/' title={APP_NAME}>{LOGO_ICON()}</RLink>
         <Stack direction='row' component='span' alignItems='center'>
           <NavigationMenu navItems={navItems} />
           <CartButton />
@@ -29,10 +31,10 @@ export default function Layout (props) {
       <Stack component='footer' gap={5} justifyContent='center' alignItems='center' style={{ gridRow: 'last', paddingBottom: 10, backgroundColor: '#8d8fe9', borderTopRightRadius: '72px' }}>
         <Stack direction='row' gap={5} justifyContent='flex-start' alignItems='center'>
           {LOGO_ICON(96, 96)}
-          <Typography variant='h6' sx={{ fontStyle: 'italic' }}>masspow</Typography>
+          <Typography variant='h6' sx={{ fontStyle: 'italic' }}>{APP_NAME}</Typography>
         </Stack>
         <div>
-          <Typography variant='button'>Designed and developed by <Link href='https://github.com/lftdev' target='_blank' underline='none' rel='noopener'>lftdev</Link>
+          <Typography variant='button'>{LAYOUT_FOOTER_ATTRIBUTION} <Link href='https://github.com/lftdev' target='_blank' underline='none' rel='noopener'>lftdev</Link>
           </Typography>
         </div>
       </Stack>
--- src/constants/strings.js
+++ src/constants/strings.js
@@ -1,6 +1,7 @@
 export const APP_NAME = 'masspow'
 export const MAIN_TITLE = `${APP_NAME} | Supplements, gym and sport articles`
 export const DESCRIPTION = 'Grow your body massively with our nutritional supplements! You can get your training equipment here too.'
+export const LAYOUT_FOOTER_ATTRIBUTION = 'Designed and developed by'
 export const HOME_PAGE_STRINGS = {
   mainTitle: `Welcome to ${APP_NAME}!`,
   mainDescription: 'Get advised by experts in your diet and training plannings.',
