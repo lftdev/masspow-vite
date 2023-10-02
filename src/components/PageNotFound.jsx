441886a7c1694f4f96f206bb7981d652f61b5d48
lftdev <torres.lf21@gmail.com>
2023-10-02 17:08:09 -0300

    Fix style

--- src/components/PageNotFound.jsx
+++ src/components/PageNotFound.jsx
@@ -2,7 +2,7 @@ import { Link, Typography } from '@mui/material'
 import { Link as RLink } from 'react-router-dom'
 
 const PageNotFound = () => (
-  <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+  <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: 150 }}>
     <Typography variant='h1'>Error 404: not found</Typography>
     <Link to='/' component={RLink}>Take me home</Link>
   </div>
