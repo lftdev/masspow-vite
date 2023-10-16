2565c327e8b575c6695c4ecfb06b61ae4123d2e6
lftdev <torres.lf21@gmail.com>
2023-10-16 09:42:22 -0300

    Add new products

--- src/services/fetch-products.js
+++ src/services/fetch-products.js
@@ -1,7 +1,9 @@
 const PRODUCTS = [
   { id: 1, name: 'Whey Protein', price: 12, brand: 'Gold Standard', stock: 10, category: 'proteins', description: 'Milk whey has the most complete type of protein. It has essential amino acids that your body needs to different biological processes, including, of course, muscles building.' },
   { id: 2, name: 'Monohydrate Creatine', price: 20, brand: 'IDN', stock: 50, category: 'creatines', description: 'After a few weeks consuming it, creatine will help you increase your strength and your muscles recovery.' },
-  { id: 3, name: 'C4 Pre-workout', price: 10, brand: 'Cellucor', stock: 20, category: 'pre-workouts', description: 'Improves your strength, resistance and your general performance for a training session.' }
+  { id: 3, name: 'C4 Pre-workout', price: 10, brand: 'Cellucor', stock: 20, category: 'pre-workouts', description: 'Improves your strength, resistance and your general performance for a training session.' },
+  { id: 4, name: 'Slash Pre-workout', price: 15, brand: 'IDN', stock: 25, category: 'pre-workouts', description: 'Improves your strength, resistance and your general performance for a training session.' },
+  { id: 5, name: 'Whey Protein', price: 8, brand: 'HardCore', stock: 30, category: 'proteins', description: 'Concentrate whey protein that helps you cover your diary protein needs.' }
 ]
 
 export const fetchProducts = () => new Promise(resolve => setTimeout(() => resolve(PRODUCTS), 2000))
